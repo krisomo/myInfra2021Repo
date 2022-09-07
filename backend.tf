beb1e83cd24615b04497a3664fdc880809a7d462
krisomo <110426467+krisomo@users.noreply.github.com>
2022-09-07 17:38:49 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mysepte-tf-state-buket"
     key = "main"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "my-dynamodb-table"
   }
 }
